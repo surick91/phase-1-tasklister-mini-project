5ad29bb35ad436e3d97b17e29885bcd87ddb1813
sam <surickj@gmail.com>
2021-05-24 16:14:08 -0700

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  Event.preventDefault()
+
 });
+
+const newForm = document.getElementById("create-task-form") 
+newForm.addEventListener('submit',(Event) => {
+  Event.preventDefault()
+  const newList = document.createElement("li")
+   newList.textContent=Event.target["new-task-description"].value
+   const taskList = document.getElementById('tasks')
+   taskList.append(newList)
+})
+
+
